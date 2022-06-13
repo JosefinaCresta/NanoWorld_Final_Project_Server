7bb150298d30925a645cc63ff6067bfe8a1f7da6
JosefinaCresta <josefinacresta@gmail.com>
2022-06-13 12:02:33 -0300

    update Project

--- src/main/java/com/ironhack/finalprojectserver/service/impl/ProjectService.java
+++ src/main/java/com/ironhack/finalprojectserver/service/impl/ProjectService.java
@@ -1,6 +1,9 @@
 package com.ironhack.finalprojectserver.service.impl;
 
+import com.ironhack.finalprojectserver.DTO.ProjectDTO;
+import com.ironhack.finalprojectserver.model.Calculator;
 import com.ironhack.finalprojectserver.model.Project;
+import com.ironhack.finalprojectserver.repository.CalculatorRepository;
 import com.ironhack.finalprojectserver.repository.ProjectRepository;
 import com.ironhack.finalprojectserver.service.interfaces.ProjectServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,9 @@ public class ProjectService implements ProjectServiceInterface {
 
     @Autowired
     private ProjectRepository projectRepository;
+
+    @Autowired
+    private CalculatorRepository calculatorRepository;
 
     public Project findById(Long id) {
         return projectRepository.findById(id).orElseThrow(() -> new ResponseStatusException(
@@ -32,11 +38,29 @@ public class ProjectService implements ProjectServiceInterface {
         projectRepository.save(project);
     }
 
-    public void update(Long id, Project project) {
+   /* public void update(Long id, Project project) {
         Project projectFromDB = projectRepository.findById(id).orElseThrow(() -> new ResponseStatusException(
                 HttpStatus.NOT_FOUND, "Project is not found"));
-        project.setId(projectFromDB.getId());
-        projectRepository.save(project);
+
+        project.setCalculator(projectFromDB.getCalculator());
+        projectRepository.save(projectFromDB);
+    }*/
+
+
+    public void update(Long id, Project project) {
+        Optional<Project> foundProject = projectRepository.findById(id);
+        if(foundProject.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No Project found with that ID");
+        } else {
+            if(!project.getTitle().isEmpty()){
+                foundProject.get().setTitle(project.getTitle());
+                foundProject.get().setDescription(project.getDescription());
+                foundProject.get().setCalculator(foundProject.get().getCalculator());
+                projectRepository.save(foundProject.get());
+            } else {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Author Name cannot be blank");
+            }
+        }
     }
 
     public void deleteProject(Long id) {
